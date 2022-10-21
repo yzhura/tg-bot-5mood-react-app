a6a28af4c12e13b3ddd19d9b7818eb939ddcb877
yzhura <zhuravlev.yaroslav@gmail.com>
2022-10-21 14:21:12 +0300

    telegram.ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import { ProductList } from './components/ProductList/ProductList';
 import { useTelegram } from './hooks/useTelegram';
 
+// eslint-disable-next-line no-undef
+const telegram = Telegram.WebApp;
+
 function App() {
+
+  useEffect(() => {
+    telegram.ready()
+  }, [])
 
   const {user, onClose} = useTelegram();
 
