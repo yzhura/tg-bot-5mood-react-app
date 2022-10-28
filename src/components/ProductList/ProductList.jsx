7f61ab6c88738b3cbb29813417fa38a54c94912b
yzhura <zhuravlev.yaroslav@gmail.com>
2022-10-28 17:33:17 +0300

    add animation

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -14,11 +14,12 @@ import {
 } from "@mui/material";
 import ViewAgendaIcon from "@mui/icons-material/ViewAgenda";
 import ViewColumnIcon from "@mui/icons-material/ViewColumn";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import corsetImg from "../../images/corset.jpg";
 import "./Product.css";
 import { useTelegram } from "../../hooks/useTelegram";
+import { useSpring, animated, useTransition, useSpringRef, useChain } from "react-spring";
 
 const staticImg = corsetImg;
 
@@ -143,80 +144,105 @@ export const ProductList = () => {
     setXsCols(!xsCols);
   };
 
+  const transApi = useSpringRef();
+  const transition = useTransition(productsItems, {
+    ref: transApi,
+    trail: 400 / productsItems.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 }
+  });
+
+  useChain([transApi], [0, 0.6])
+
+  const gridBtnStyles = useSpring({
+    from: { right: -150 },
+    to: { right: 0 },
+  });
+
   return (
     <>
       {/* <Button onClick={onSendData}>Test Post</Button> */}
       <Box padding={1} display="flex" justifyContent="flex-end">
-        <IconButton onClick={toggleXsCols}>
-          {xsCols ? <ViewColumnIcon /> : <ViewAgendaIcon /> }
-        </IconButton>
+        <animated.div
+          style={{
+            position: "relative",
+            ...gridBtnStyles,
+          }}
+        >
+          <IconButton onClick={toggleXsCols}>
+            {xsCols ? <ViewColumnIcon /> : <ViewAgendaIcon />}
+          </IconButton>
+        </animated.div>
       </Box>
       <Grid container spacing={2}>
-        {productsItems.map(({ id, title, price, oldprice, img, count }) => {
+        {transition((style, { id, title, price, oldprice, img, count }) => {
           const isMoreThanZero = count > 0;
           return (
             <Grid item xs={xsCols ? 12 : 6} md={4} key={id}>
-              <Box position="relative">
-                <Card>
-                  <Box position="absolute" right="0" top="-10px">
-                    <Badge badgeContent={count} color="primary"/>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    height="175"
-                    image={img}
-                    alt={title}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="p">
-                      {title}
-                    </Typography>
-                    {oldprice && (
-                      <Typography variant="body2" color="text.secondary">
-                        <span
-                          style={{
-                            textDecoration: "line-through",
-                          }}
-                        >
-                          {oldprice} грн.
-                        </span>
-                      </Typography>
-                    )}
-                    <Typography variant="body2" color="text.secondary">
-                      <strong>{price} грн.</strong>
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Box
-                      width="100%"
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      {isMoreThanZero ? (
-                        <>
-                          <ButtonGroup>
-                            <Button onClick={() => onDeleteHandler(id)}>
-                              <RemoveIcon fontSize="medium" />
-                            </Button>
-                            <Button onClick={() => onAddHandler(id)}>
-                              <AddIcon fontSize="medium" />
-                            </Button>
-                          </ButtonGroup>
-                        </>
-                      ) : (
-                        <Button
-                          fullWidth
-                          variant="contained"
-                          onClick={() => onAddHandler(id)}
-                        >
-                          Додати
-                        </Button>
-                      )}
+              <animated.div style={{ ...style }}>
+                <Box position="relative">
+                  <Card>
+                    <Box position="absolute" right="0" top="-10px">
+                      <Badge badgeContent={count} color="primary" />
                     </Box>
-                  </CardActions>
-                </Card>
-              </Box>
+                    <CardMedia
+                      component="img"
+                      height="175"
+                      image={img}
+                      alt={title}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="p">
+                        {title}
+                      </Typography>
+                      {oldprice && (
+                        <Typography variant="body2" color="text.secondary">
+                          <span
+                            style={{
+                              textDecoration: "line-through",
+                            }}
+                          >
+                            {oldprice} грн.
+                          </span>
+                        </Typography>
+                      )}
+                      <Typography variant="body2" color="text.secondary">
+                        <strong>{price} грн.</strong>
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Box
+                        width="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        {isMoreThanZero ? (
+                          <>
+                            <ButtonGroup>
+                              <Button onClick={() => onDeleteHandler(id)}>
+                                <RemoveIcon fontSize="medium" />
+                              </Button>
+                              <Button onClick={() => onAddHandler(id)}>
+                                <AddIcon fontSize="medium" />
+                              </Button>
+                            </ButtonGroup>
+                          </>
+                        ) : (
+                          <Button
+                            fullWidth
+                            variant="contained"
+                            onClick={() => onAddHandler(id)}
+                          >
+                            Додати
+                          </Button>
+                        )}
+                      </Box>
+                    </CardActions>
+                  </Card>
+                </Box>
+              </animated.div>
             </Grid>
           );
         })}
